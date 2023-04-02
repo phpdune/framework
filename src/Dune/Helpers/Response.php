a9e9a5384bc9e60b4c655e4e709f3bffbd344751
coderswat <coderswat@gmail.com>
2023-04-02 16:55:52 +0530

    added class Response

---
+++ src/Dune/Helpers/Response.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dune\Helpers;
+
+class Response 
+{
+  public function json(array $data,int $code = 200): ?string
+  {
+      $data = json_encode($data);
+      header('Content-Type:application/json');
+       http_response_code($code); 
+       echo $data; 
+       exit;
+  }
+}

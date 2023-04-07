47aea9a1f6ffe6217b624104abf9c48c1b6ef835
coderswat <coderswat@gmail.com>
2023-04-08 01:19:44 +0530

    added logger for custom logs

---
+++ src/Dune/ErrorHandler/Logger.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dune\ErrorHandler;
+
+class Logger 
+{
+    /**
+     * error log file setting
+     *
+     * @param none
+     *
+     * @return none
+     */    
+   public function __construct() 
+   {
+        ini_set("log_errors", 'On');
+        ini_set('error_log',$this->logFile());
+   }
+    /**
+     * will log the message
+     *
+     * @param string $message
+     *
+     * @return none
+     */    
+   public function put(string $message): void
+   {
+     error_log($message);
+   }
+    /**
+     * return app.log path
+     *
+     * @param none
+     *
+     * @return string
+     */    
+   public function logFile(): string
+   {
+     return PATH.'/storage/log/app.log';
+   }
+}

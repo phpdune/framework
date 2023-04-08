42d455434e7c2377d4d7bcde12c85e89afa10334
coderswat <coderswat@gmail.com>
2023-04-09 00:26:05 +0530

    added App.php

---
+++ src/Dune/App.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dune; 
+
+use Dotenv\Dotenv;
+
+class App 
+{
+     /**
+      * load the env variables and set custom error handling
+      * 
+      * @param  none
+      *
+      * @return none
+      */
+  public function __construct()
+  {
+    $dotenv = Dotenv::createImmutable(PATH);
+    $dotenv->load();
+    set_error_handler('errorHandler', E_ALL);
+    set_exception_handler('exceptionHandler');
+  }
+     /**
+      * load apo configuration
+      * 
+      * @param  none
+      *
+      * @return none
+      */
+  public function load(): void
+  {
+    $this->loadAppConfig();
+  }
+     /**
+      * load apo configuration
+      * 
+      * @param  none
+      *
+      * @return none
+      */  
+  public function loadAppConfig(): void
+  {
+    date_default_timezone_set(config('app.timezone'));
+  }
+
+}

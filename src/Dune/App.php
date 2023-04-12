0105f98c57b7310dbeaa1b5230ae6241f5074034
coderswat <coderswat@gmail.com>
2023-04-12 17:53:19 +0530

    App php updated

--- src/Dune/App.php
+++ src/Dune/App.php
@@ -2,46 +2,50 @@
 
 declare(strict_types=1);
 
-namespace Dune;
+namespace Dune; 
 
 use Dotenv\Dotenv;
 
-final class App
+final class App 
 {
-    /**
-     * load the env variables and set custom error handling
-     *
-     * @param  none
-     *
-     * @return none
-     */
-    public function __construct()
-    {
-        $dotenv = Dotenv::createImmutable(PATH);
-        $dotenv->load();
-        set_error_handler('errorHandler', E_ALL);
-        set_exception_handler('exceptionHandler');
-    }
-       /**
-        * load apo configuration
-        *
-        * @param  none
-        *
-        * @return none
-        */
-    public function load(): void
-    {
-        $this->loadAppConfig();
-    }
-       /**
-        * load apo configuration
-        *
-        * @param  none
-        *
-        * @return none
-        */
-    public function loadAppConfig(): void
-    {
-        date_default_timezone_set(config('app.timezone'));
-    }
+     /**
+      * load the env variables and set custom error handling
+      * 
+      * @param  none
+      *
+      * @return none
+      */
+  public function __construct()
+  {
+    $dotenv = Dotenv::createImmutable(PATH);
+    $dotenv->load();
+    set_error_handler('errorHandler', E_ALL);
+    set_exception_handler('exceptionHandler');
+    
+  }
+     /**
+      * load apo configuration
+      * 
+      * @param  none
+      *
+      * @return none
+      */
+  public function load(): void
+  {
+    $this->loadAppConfig();
+    require PATH.'/routes/web.php';
+    echo runRoutes();
+  }
+     /**
+      * load apo configuration
+      * 
+      * @param  none
+      *
+      * @return none
+      */  
+  public function loadAppConfig(): void
+  {
+    date_default_timezone_set(config('app.timezone'));
+  }
+
 }

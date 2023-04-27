1c2836cb661ae1411ea3f41f8dedfa6f6d602496
coderswat <coderswat@gmail.com>
2023-04-28 04:02:24 +0530

    http request bug fix

--- src/Dune/Http/Request.php
+++ src/Dune/Http/Request.php
@@ -7,16 +7,23 @@ namespace Dune\Http;
 use Dune\Helpers\Redirect;
 use Dune\Http\Validater;
 use Dune\Http\ValidaterInterface;
+use Dune\Http\RequestInterface;
 
 class Request implements RequestInterface
 {
     use RequestContainer;
     /**
-     * validater instance
+     * Validation error message storred here
      *
-     * @var Validater
+     * @var array<string,string>
      */
-    private ?Validater $validater = null;
+    private array $errors = [];
+    /**
+      * old value of form fields stored here
+      *
+      * @var array<string,mixed>
+      */
+    private array $oldInputs = [];
 
     /**
       * All data from $_REQUEST and $_SERVER
@@ -37,7 +44,6 @@ class Request implements RequestInterface
      */
     public function __construct()
     {
-        $this->init();
         if ($this->isGet()) {
             foreach ($_GET as $key => $value) {
                 $this->data[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
@@ -120,15 +126,6 @@ class Request implements RequestInterface
      {
          return $this->server('remote_addr');
      }
-    /**
-     * @param  ValidaterInterface|array<mixed> $data
-     *
-     * @return ?Redirect
-     */
-     public function validate(array|ValidaterInterface $data): ?Redirect
-     {
-         return $this->validater->validate($data);
-     }
 
     /**
      * @param array<string,mixed> $params
@@ -146,4 +143,91 @@ class Request implements RequestInterface
      {
          return (isset($this->params[$key]) ? $this->params[$key] : null);
      }
+    /**
+    * request validation
+    * request input validation
+    *
+    * @param ValidaterInterface|array<mixed> $data
+    *
+    * @return ?Redirect
+    */
+    public function validate(ValidaterInterface|array $data): ?Redirect
+    {
+        if (is_array($data)) {
+            $rules = $data;
+        } else {
+            $rules = $data->validation();
+        }
+        foreach ($rules as $field => $rule) {
+            foreach ($rule as $ruleName => $ruleValue) {
+                if ($this->get($field)) {
+                    $this->oldInputs['old_'.$field] = $this->get($field);
+                }
+
+                ($ruleName ? $this->checkValidation($ruleName, $ruleValue, $field) : $this->checkValidation($ruleValue, $ruleValue, $field));
+            }
+        }
+        if (!empty($this->errors)) {
+            if (!$this->oldInputs) {
+                return redirect()->back()->withArray($this->errors);
+            }
+
+            return redirect()->back()->withArray(array_merge($this->errors, $this->oldInputs));
+        }
+        return null;
+    }
+    /**
+    * @param  string $name
+    * @param mixed $ruleValue
+    * @param string $field
+    *
+    */
+    private function checkValidation(string $name, mixed $ruleValue, string $field): void
+    {
+        $value = $this->get($field) ?? '';
+        if ($name == 'required') {
+            if (!$value) {
+                if (!$this->errors[$field]) {
+                    $this->errors[$field] = "field is required";
+                }
+            }
+        } elseif ($name == 'min') {
+            if (strlen($value) < $ruleValue) {
+                if (!$this->errors[$field]) {
+                    $this->errors[$field] = "field must be at least {$ruleValue} characters";
+                }
+            }
+        } elseif ($name == 'max') {
+            if (strlen($value) > $ruleValue) {
+                if (!$this->errors[$field]) {
+                    $this->errors[$field] = "field may not be greater than {$ruleValue} characters";
+                }
+            }
+        } elseif ($name == 'email') {
+            if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+                if (!$this->errors[$field]) {
+                    $this->errors[$field] = "field must be a valid email address";
+                }
+            }
+        } elseif ($name == 'equal') {
+            if ($value !== $this->get($ruleValue)) {
+                if (!$this->errors[$field]) {
+                    $this->errors[$field] = "field must be equal to {$ruleValue}";
+                }
+            }
+        } elseif ($name == 'numeric') {
+            if (!ctype_digit($value)) {
+                if (!$this->errors[$field]) {
+                    $this->errors[$field] = 'field must be numeric';
+                }
+            }
+        } elseif ($name == 'digit') {
+            $num = explode(',', $ruleValue);
+            if ($value < $num[0] || $value > $num[1]) {
+                if (!$this->errors[$field]) {
+                    $this->errors[$field] = "field must be digit between {$num[0]} to {$num[1]}";
+                }
+            }
+        }
+    }
 }

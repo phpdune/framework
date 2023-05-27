07ca01834b8c4f6367e233aed622db56a22e36ff
coderswat <coderswat@gmail.com>
2023-05-27 17:24:49 +0530

    cookie facade added

---
+++ src/Dune/Facades/Cookie.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of Dune Framework.
+ *
+ * (c) Abhishek B <phpdune@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Dune\Facades;
+
+use Dune\Facades\Facade;
+
+class Cookie extends Facade
+{
+    /**
+     * accesser to resolve the specific instance
+     *
+     * @return string
+     */
+    protected static function getAccessor(): string
+    {
+        return 'cookie';
+    }
+}
